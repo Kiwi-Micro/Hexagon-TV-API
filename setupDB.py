38956f3d7a15ac5a858e680650fb3f40f4ad9a32
Malcolm <hauser.malcolm@gmail.com>
2024-12-08 11:20:30 +1000

    Big Changes Comming Soon

--- setupDB.py
+++ setupDB.py
@@ -1,63 +1,27 @@
 # Made by Malcolm @ Hexagon TV 2024
 import mysql.connector
 
-configPath = "/hexagontv/password.txt"
+passwordFilePath = "/hexagontv/password.txt"
 
 try:
-	with open(configPath, 'r') as file:
+	with open(passwordFilePath, 'r') as file:
 		dbPassword = file.readline().strip()
 except FileNotFoundError:
-	raise RuntimeError(f"Configuration file not found at {configPath}")
+	raise RuntimeError(f"Password File Not Found At {passwordFilePath}")
 
 try:
-	moviesConnection = mysql.connector.connect(
+	connection = mysql.connector.connect(
 		host="localhost",
 		user="hexagon",
 		password=dbPassword,
-		database="hexagonMoviedb"
+		database="hexagonTVdb"
 	)
-	usersConnection = mysql.connector.connect(
-		host="localhost",
-		user="hexagon",
-		password=dbPassword,
-		database="hexagonUsersdb"
-	)
-	usersCursor = usersConnection.cursor()
-	moviesCursor = moviesConnection.cursor()
+	cursor = connection.cursor()
 except mysql.connector.Error as err:
 	raise RuntimeError(f"Database connection failed: {err}")
 
-# --- Table Creation ---
-# --- Movies Setup ---
-
-moviesCursor.execute("""
-CREATE TABLE IF NOT EXISTS movies (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(255) NOT NULL,
-	description TEXT,
-	thumbnailURL VARCHAR(255),
-	videoURL VARCHAR(255),
-	date DATE,
-	urlName VARCHAR(255) UNIQUE,
-	rating VARCHAR(10),
-	category VARCHAR(50)
-);
-""")
-moviesCursor.execute("""
-CREATE TABLE IF NOT EXISTS tvshows (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(255) NOT NULL,
-	description TEXT,
-	thumbnailURL VARCHAR(255),
-	videoURL VARCHAR(255),
-	date DATE,
-	urlName VARCHAR(255) UNIQUE,
-	rating VARCHAR(10),
-	category VARCHAR(50)
-);
-""")
-moviesCursor.execute("""
-CREATE TABLE IF NOT EXISTS documentaries (
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS videos (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(255) NOT NULL,
 	description TEXT,
@@ -70,23 +34,22 @@ CREATE TABLE IF NOT EXISTS documentaries (
 );
 """)
 
-# --- Users Setup ---
-
-usersCursor.execute("""
+cursor.execute("""
 CREATE TABLE IF NOT EXISTS users (
 	username VARCHAR(32) PRIMARY KEY,
 	passwordCheckSum LONGTEXT,
-	email VARCHAR(255)
+	email VARCHAR(255),
+	isAdmin BOOLEAN DEFAULT false
 )
 """)
-usersCursor.execute("""
+cursor.execute("""
 CREATE TABLE IF NOT EXISTS sessions (
 	username VARCHAR(32),
 	sessionUUID CHAR(36) PRIMARY KEY,
 	expires DATE
 )
 """)
-usersCursor.execute("""
+cursor.execute("""
 CREATE TABLE IF NOT EXISTS watchlist (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	username VARCHAR(255) NOT NULL,
@@ -95,7 +58,7 @@ CREATE TABLE IF NOT EXISTS watchlist (
 	thumbnailURL VARCHAR(255) NOT NULL
 )
 """)
-usersCursor.execute("""
+cursor.execute("""
 CREATE TABLE IF NOT EXISTS continueWatching (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	username VARCHAR(255) NOT NULL,
