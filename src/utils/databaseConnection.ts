970f12b129a0571f779de44d58fa2f16afff3223
Malcolm <hauser.malcolm@gmail.com>
2025-01-10 17:22:50 +1000

    Maybe

--- src/utils/databaseConnection.ts
+++ src/utils/databaseConnection.ts
@@ -1,9 +1,8 @@
 import { createClient } from "@libsql/client";
-import config from "../../config.json";
 
-const TURSO_DB_URL = config[0]["TURSO_DB_URL"];
-const TURSO_DB_AUTH_TOKEN_R = config[0]["TURSO_DB_AUTH_TOKEN_R"];
-const TURSO_DB_AUTH_TOKEN_RW = config[0]["TURSO_DB_AUTH_TOKEN_RW"];
+const TURSO_DB_URL = process.env.TURSO_DB_URL || "";
+const TURSO_DB_AUTH_TOKEN_R = process.env.TURSO_DB_AUTH_TOKEN_R || "";
+const TURSO_DB_AUTH_TOKEN_RW = process.env.TURSO_DB_AUTH_TOKEN_RW || "";
 
 const TURSO_DB_CONNECTION_R = createClient({
 	url: TURSO_DB_URL,
