c95d19d7a6c3059e6a9f0ee63039937139f7aaf9
Malcolm <hauser.malcolm@gmail.com>
2025-01-10 17:26:22 +1000

    no

--- src/utils/databaseConnection.ts
+++ src/utils/databaseConnection.ts
@@ -4,8 +4,6 @@ const TURSO_DB_URL = process.env.TURSO_DB_URL || "";
 const TURSO_DB_AUTH_TOKEN_R = process.env.TURSO_DB_AUTH_TOKEN_R || "";
 const TURSO_DB_AUTH_TOKEN_RW = process.env.TURSO_DB_AUTH_TOKEN_RW || "";
 
-console.log(TURSO_DB_URL);
-
 const TURSO_DB_CONNECTION_R = createClient({
 	url: TURSO_DB_URL,
 	authToken: TURSO_DB_AUTH_TOKEN_R,
