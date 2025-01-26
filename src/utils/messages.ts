9a87ca61938de2456438ee7c18a4d8246aa3e758
Malcolm <hauser.malcolm@gmail.com>
2025-01-26 18:00:43 +1000

    log ip

--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -1,8 +1,8 @@
 function printEndpointReached(req: any, res: any) {
 	console.log(
-		`${new Date().toString().slice(4, 24)} GMT+0000: ${req.method} ${
-			req.url
-		} ${res.statusCode}`,
+		`${req.socket.remoteAddress}: ${new Date()
+			.toString()
+			.slice(4, 24)} GMT+0000: ${req.method} ${req.url} ${res.statusCode}`,
 	);
 }
 
